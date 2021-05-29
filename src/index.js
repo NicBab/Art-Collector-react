7cdf535475a22a4cfe45362a7f66977c45ab2c3a
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-05-29 09:45:05 -0500

    art collector commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  Feature,
+  Loading,
+  Preview,
+  Search,
+  Title
+} from './components';
+
+
+  /**
+   * We are at the App level component, which is top-most. Any state which needs to be shared between immediate children should
+   * be made here, so create state pairs using useState() for:
+   * searchResults, setSearchResults (default should be this object:  {info: {}, records: []} )
+   * featuredResult, setFeaturedResult (default should be null)
+   * isLoading, setIsLoading (default should be false)
+   */
+const App = () => {
+  const [searchResults, setSearchResults] = useState({info: {}, records: []} );
+  const [featuredResult, setFeaturedResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+   return <div className="app">
+
+     {/* <Title /> is static, doesn't need any props */}
+     <Title />
+     
+     {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
+     <Search 
+      setSearchResults = {setSearchResults}
+      setIsLoading = {setIsLoading} />
+
+     {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
+     <Preview 
+      searchResults = {searchResults} 
+      setSearchResults = {setSearchResults}
+      setIsLoading = {setIsLoading}
+      setFeaturedResult = {setFeaturedResult} /> 
+
+     {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties)*/}
+     <Feature
+      featuredResults = {setFeaturedResults}
+      setIsLoading = {setIsLoading}
+      setSearchResults = {setSearchResults} /> 
+
+     {/* <Loading /> is static, but should only render when isLoading is true */}
+     {/* use a ternary and render null if isLoading is false */}
+     {isLoading ? <Loading /> : null}
+   </div>
+ }
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
